1d1e232be2ee46a966fb00490806964d055d8d9b
VasilMerdzhanov <sartarus@yahoo.com>
2023-05-15 22:58:48 +0300

    Update UnSuccessfulLoginDataDrivenLevel1Test.java

--- src/test/java/com/in28minutes/datadriventests/UnSuccessfulLoginDataDrivenLevel1Test.java
+++ src/test/java/com/in28minutes/datadriventests/UnSuccessfulLoginDataDrivenLevel1Test.java
@@ -11,13 +11,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 
 public class UnSuccessfulLoginDataDrivenLevel1Test {
-	//String[] userIds = {"sartarus@mail.bg", "spark@mail.bg", "noname@mail.bg"};
+	//String[] userIds = {"*******@mail.bg", "spark@mail.bg", "noname@mail.bg"};
 	
 	//Create the Data Provider and give the data provider a name
 	@DataProvider(name="user-ids-password-data-provider")
 	public String[][] usersIdsAndPasswordsDataProvider() {
 		return new String[][]{
-				{"sartarus@mail.bg", "hugoboss"},
+				{"********@mail.bg", "*******"},
 				{"spark@mail.bg", "neshto"},
 				{"greshen@mail.bg","neshto"}
 				};
@@ -30,7 +30,7 @@ public class UnSuccessfulLoginDataDrivenLevel1Test {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mythos\\Desktop\\Drivers\\chromedriver.exe"); 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://mail.bg/auth/lgn");
-		//driver.findElement(By.id("imapuser")).sendKeys("sartarus@mail.bg");
+		//driver.findElement(By.id("imapuser")).sendKeys("******@mail.bg");
 		driver.findElement(By.id("imapuser")).sendKeys(userIds);
 		WebElement passwordElement = driver.findElement(By.id("pass"));
 		passwordElement.sendKeys(password);	
@@ -39,7 +39,7 @@ public class UnSuccessfulLoginDataDrivenLevel1Test {
 		
 //		String errorMessageText = driver.findElement(By.id("footer_left")).getText();
 //		System.out.println(errorMessageText);
-//		assertEquals(errorMessageText,"© 2022 Mail.bg");
+//		assertEquals(errorMessageText,"Â© 2022 Mail.bg");
 	
 		driver.quit();
 		

34c58779c607d2dc67b6f1cab95e2880575298d5
VasilMerdzhanov <sartarus@yahoo.com>
2023-05-15 23:04:38 +0300

    Update LoginDataProviderCompliteTest.java

--- src/test/java/com/in28minutes/datadriventests/LoginDataProviderCompliteTest.java
+++ src/test/java/com/in28minutes/datadriventests/LoginDataProviderCompliteTest.java
@@ -9,13 +9,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 
 public class LoginDataProviderCompliteTest {
-	//String[] userIds = {"sartarus@mail.bg", "spark@mail.bg", "noname@mail.bg"};
+	//String[] userIds = {"******@mail.bg", "spark@mail.bg", "noname@mail.bg"};
 	
 	//Create the Data Provider and give the data provider a name
 	@DataProvider(name="user-ids-password-data-provider")
 	public Object[][] usersIdsAndPasswordsDataProvider() {
 		return new Object[][]{
-				{"sartarus@mail.bg", "hugoboss", true},
+				{"8888@mail.bg", "*****", true},
 				{"spark@mail.bg", "neshto", false},
 				{"greshen@mail.bg","neshto", false}
 				};
@@ -28,7 +28,7 @@ public class LoginDataProviderCompliteTest {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mythos\\Desktop\\Drivers\\chromedriver.exe"); 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://mail.bg/auth/lgn");
-		//driver.findElement(By.id("imapuser")).sendKeys("sartarus@mail.bg");
+		//driver.findElement(By.id("imapuser")).sendKeys("*****@mail.bg");
 		driver.findElement(By.id("imapuser")).sendKeys(userIds);
 		WebElement passwordElement = driver.findElement(By.id("pass"));
 		passwordElement.sendKeys(password);	
@@ -38,11 +38,11 @@ public class LoginDataProviderCompliteTest {
 		
 		if(isLoginExpectedToBeSuccessful) {
 //			String errorMessageText = driver.findElement(By.id("footer_left")).getText();
-//			assertEquals(errorMessageText,"© 2022 Mail.bg");
+//			assertEquals(errorMessageText,"Â© 2022 Mail.bg");
 		}else {
 //			String errorMessageText = driver.findElement(By.id("footer_left")).getText();      
 //			--  dava mi greshka pri namiraneto na tozi element !
-//			assertEquals(errorMessageText,"© 2022 Mail.bg");
+//			assertEquals(errorMessageText,"Â© 2022 Mail.bg");
 		}
 		
 		driver.quit();
